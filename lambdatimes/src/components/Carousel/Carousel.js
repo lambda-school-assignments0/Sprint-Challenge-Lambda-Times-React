823f7161c2824bbbcb3c70899e8e6cf33bd50d50
schang1146 <sammychang2185@gmail.com>
2019-06-03 18:55:38 -0700

    finished stretch goal: carousel

--- lambdatimes/src/components/Carousel/Carousel.js
+++ lambdatimes/src/components/Carousel/Carousel.js
@@ -5,28 +5,48 @@ export default class Carousel extends Component {
   constructor(props){
     super(props);
     this.state = {
-
+      selectedImg: 0,
+      imgs: [],
     }
   }
   componentDidMount(){
-
+    this.setState({
+      imgs: carouselData,
+    })
   }
 
   leftClick = () => {
-
+    if (this.state.selectedImg === 0) {
+      this.setState({
+        selectedImg: this.state.imgs.length - 1,
+      })
+    } else {
+      this.setState({
+        selectedImg: this.state.selectedImg - 1,
+      })
+    }
   }
 
   rightClick = () => {
-
+    if (this.state.selectedImg === this.state.imgs.length) {
+      this.setState({
+        selectedImg: 0,
+      })
+    } else {
+      this.setState({
+        selectedImg: this.state.selectedImg + 1,
+      })
+    }
   }
 
   selectedImage = () => {
-    return <img src={} style={{display: 'block'}} />
+    return <img alt={this.state.imgs[this.state.selectedImg]} src={this.state.imgs[this.state.selectedImg]} style={{display: 'block'}} />
   }
   
   render(){
     return (
       <div className="carousel">
+        {this.selectedImage()}
         <div className="left-button" onClick={this.leftClick}>{"<"}</div>
         <div className="right-button" onClick={this.rightClick}>{">"}</div>
       </div>
